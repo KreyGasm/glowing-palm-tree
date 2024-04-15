e5c4be8147a2fad2a16b8eebda22e60aaf5ff2b3
KreyGasm <154333970+KreyGasm@users.noreply.github.com>
2024-04-15 14:39:19 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -19,14 +19,14 @@ function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // Получаем данные пользователя из LocalStorage
+    
     var savedUser = localStorage.getItem("user");
 
     if (savedUser) {
         savedUser = JSON.parse(savedUser);
         if (username === savedUser.username && password === savedUser.password) {
             alert("Вход выполнен успешно!");
-            // Здесь можно выполнить перенаправление на другую страницу или другие действия после успешного входа
+            
         } else {
             alert("Неверное имя пользователя или пароль.");
         }
